9842dca9694e021c36115440fb1b3269f7d500c7
Rosi Eka Sakti <rosiekasakti_27@yahoo.com>
2019-05-15 23:35:00 +0700

    Update data baru

--- application/controllers/coba.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script acces allowed');
-class coba extends CI_controller{
-	public function index() {
-		$this->load->view('isi_coba');
-	}
-}
-?>
--- application/models/data.php
+++
@@ -1,7 +0,0 @@
-<?php
-class data extends CI_Model{
-    function ambil_data(){
-        return $this->db->get('perawat');
-    }
-}
-?>
