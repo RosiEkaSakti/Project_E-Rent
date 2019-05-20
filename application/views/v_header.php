25514fd29cbcfb0d81aff77d0c3dad121609c824
Aditya9880 <45248076+Aditya9880@users.noreply.github.com>
2019-05-20 22:13:36 +0700

    membuat header

---
+++ application/views/v_header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>Home</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="../assets/dist/css/template.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="../assets/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	
+<script src="../assets/vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="../assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="../assets/dist/js/template.js"></script>
+	
+	<!-- Metis Menu Plugin JavaScript -->
+    <script src="../assets/vendor/metisMenu/metisMenu.min.js"></script>
+
+	<script type="text/javascript">
+    $(document).ready(function() {
+        $("#menu-toggle").click(function(e) {
+            e.preventDefault();
+            $("#wrapper").toggleClass("toggled");
+            $("#wrapper.toggled").find("#sidebar-wrapper").find(".collapse").collapse("hide");
+        });
+    });
+    </script>
+</body>
+
+</html>
