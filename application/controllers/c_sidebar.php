3d22637fa721953ca6f13fd9620a01d094b7dfef
Rosi Eka Sakti <rosiekasakti_27@yahoo.com>
2019-05-20 23:35:39 +0700

    mulai dari awal

---
+++ application/controllers/c_sidebar.php
@@ -0,0 +1,11 @@
+<?php
+
+class c_sidebar extends CI_Controller {
+
+	public function index()
+	{
+        //memanggil file view
+        $this->load->view('v_sidebar');//file view
+    }
+}
+?>
---
+++ application/views/v_sidebar.php
@@ -0,0 +1,67 @@
+<html>
+<head>
+
+</head>
+<body>
+
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation" style="margin-bottom: 0">
+    <div class="navbar-default sidebar" role="navigation">
+        <div class="sidebar-nav navbar-collapse">
+            <ul class="nav" id="side-menu">
+				<li>
+				    <center><img src="<?php echo $root; ?>/assets/gambar/LOGO E-RENT FIX.png" class="img-responsive" width="100%"><span class="masked"> </span></center>
+				</li>
+					<li>
+                    <a href="<?php echo $root; ?>dashboard/index_admin.php"><i class="fa fa-dashboard fa-fw"></i><span class="masked"> Home </span></a>
+                    </li>
+                    <li>
+                        <a href="<?php echo $root; ?>user/index_admin.php"><i class="fa fa-user fa-fw"></i><span class="masked"> Admin </span></a>
+                    </li>
+                    <li>
+                        <a href="<?php echo $root; ?>mobil/index_admin.php"><i class="fa fa-car fa-fw"></i><span class="masked"> Mobil </span></a>
+                    </li>
+					<li>
+                        <a href="<?php echo $root; ?>pelanggan/index_admin.php"><i class="fa fa-users fa-fw"></i><span class="masked"> Pelanggan </span></a>
+                    </li>
+					<li>
+                        <a href="#"><i class="fa fa-file fa-fw"></i> Transaksi <span class="fa arrow"></span></a>
+							<ul class="nav nav-second-level">
+                                <li>
+                                    <a href="<?php echo $root; ?>biayasewa/index_admin.php">Transaksi Biaya</a>
+                                </li>
+                                <li>
+                                    <a href="<?php echo $root; ?>pembayaran/index_admin.php">Pembayaraan</a>
+                                </li>
+                                <li>
+                                    <a href="<?php echo $root; ?>pengembalian/index_admin.php">Pengembalian</a>
+                                </li>
+                            </ul> 
+                    </li>
+                    <li>
+                        <a href="<?php echo $root; ?>backupdanrestore/formbackup.php"><i class="fa fa-user fa-fw"></i><span class="masked"> Backup </span></a>
+                    </li>
+                    <li>
+                        <a href="<?php echo $root; ?>backupdanrestore/formrestore.php"><i class="fa fa-user fa-fw"></i><span class="masked"> Restore </span></a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa fa-file fa-fw"></i> Backup dan Restore <span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a href="<?php echo $root; ?>backup.php">Backup</a>
+                                </li>
+                                <li>
+                                    <a href="<?php echo $root; ?>restore.php">Restore</a>
+                                </li> -->
+                            </ul> 
+                    </li> 
+            </ul>
+        </div>
+        <!-- /.sidebar-collapse -->
+    </div>
+</nav>
+
+
+
+</body>
+</html>
+
