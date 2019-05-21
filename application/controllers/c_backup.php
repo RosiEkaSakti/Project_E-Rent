5162c8c4fc3e88b00e7c19b72bdd8c898db5522e
Rustiti <rustiti99@gmail.com>
2019-05-21 20:48:19 +0700

    menambahkan backup

---
+++ application/controllers/c_backup.php
@@ -0,0 +1,13 @@
+<?php
+
+class c_backup extends CI_Controller {
+
+	public function index(){
+        //memanggil file view
+        $data=array (
+            'sidebar' => 'v_sidebar'
+        );
+        $this->load->view('v_backup',$data);   
+    }
+}
+?>
