abd52cdb3a4c6738732465d57e0fb0d753bb133f
Rustiti <rustiti99@gmail.com>
2019-06-17 23:30:20 +0700

    model

--- application/models/m_mobil.php
+++ application/models/m_mobil.php
@@ -3,12 +3,14 @@
 class m_mobil extends CI_Model
 {
     private $_table = "tb_mobil";
+
     public $id_mobil;
     public $foto_mobil;
     public $id_jenis;
     public $type_mobil;
     public $merk;
     public $no_polisi;
+    public $warna;
     public $harga;
     public $status;  
 
@@ -100,6 +102,7 @@ class m_mobil extends CI_Model
     public function save()
     {
         $post = $this->input->post();
+<<<<<<< HEAD
         $this->id_mobil = $post["No"];
         $this->foto_mobil = $post["image"];
         $this->id_jenis = $post["no"];
@@ -108,19 +111,39 @@ class m_mobil extends CI_Model
         $this->no_polisi = $post["label"];
         $this->harga = $post["no"];
         $this->status = $post["label"];
+=======
+        $this->id_mobil = $post["id_mobil"];
+        $this->foto_mobil = $post["foto_mobil"];
+        $this->type_mobil = ($post["type_mobil"]);
+        $this->id_jenis = ($post["id_jenis"]);
+        $this->merk = ($post["merk"]);
+        $this->no_polisi = ($post["no_polisi"]);
+        $this->warna = ($post["warna"]);
+        $this->harga = ($post["harga"]);
+        $this->status = ($post["status"]);
+>>>>>>> 5963590502f696ee1e041e01da669fbe18609053
         $this->db->insert($this->_table, $this);
     }
 
     public function update($user)
     {
         $post = $this->input->post();
+<<<<<<< HEAD
         $this->id_mobil = $post["id_mobil"];
         $this->db->update($this->_table, $user, array("id_mobil" => $id['id_mobil']));
+=======
+        $this->id_mobil = $post["id"];
+        $this->db->update($this->_table, $user, array("id_mobil" => $post['id']));
+>>>>>>> 5963590502f696ee1e041e01da669fbe18609053
     }
 
     public function delete($id)
     {
+<<<<<<< HEAD
         return $this->db->delete($this->_table, array("id_mobil" => $id['id_mobil']));
+=======
+        return $this->db->delete($this->_table, array("id_mobil" => $id));
+>>>>>>> 5963590502f696ee1e041e01da669fbe18609053
     }
 
     public function get_view(){
